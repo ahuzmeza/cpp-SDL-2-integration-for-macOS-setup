ef658ced20c048925538e1acca00134bed919446
Alin <alinhuzmezan@yahoo.com>
2022-12-15 16:20:52 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # cpp-SDL-2-integration-for-macOS-setup
+
+Setup: 
+
+    Use 'sh run.sh' 
+    - To create project files
+    - Download dependencies
+    - Link dependencies
+
+Project entrypoint:
+
+    dir src/
+
+    main.cpp is the project's entry point
+
+How to build and run?
+
+    Use Makefile commands:
+    
+    'make'     - to build project
+    'make run' - to build and run
+
---
+++ run.sh
@@ -0,0 +1,28 @@
+echo "---------------------------------------------------------------------"
+echo "--- Updating homebrew ---"
+
+brew upgrade
+brew update
+
+echo "--- installing packages ---"
+
+brew install sdl2
+brew install sdl2_image
+
+echo "--- Creating files and folders ---"
+
+mkdir -p build/debug include src lib images
+touch ./src/main.cpp
+
+echo " - Linking SDL headers and libraries ---"
+echo "Linking sdl2/include"
+ln -s /usr/local/Cellar/sdl2/2.26.1/include/SDL2 ./include/SDL2
+
+echo "Linking sdl2_image/include"
+ln -s /usr/local/Cellar/sdl2_image/2.6.2/include/SDL2 ./include/SDL2_IMAGE
+
+echo "Linking sdl2_image/lib"
+ln -s /usr/local/Cellar/sdl2_image/2.6.2/lib ./lib/SDL2_IMAGE
+
+echo "---------------------------------------------------------------------"
+
